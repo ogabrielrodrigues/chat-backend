8c1899d8da95b9917eb06647c5ecf4cfad5bfdbc
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-05 16:27:31 -0300

    feat: added socket.io

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,19 @@ import 'dotenv/config'
 import { logger } from '@helpers/logger/log'
 import cors from 'cors'
 import express from 'express'
+import { createServer } from 'node:http'
+import { Server } from 'socket.io'
 
 import { routes } from './routes'
 
 const app = express()
+const http = createServer(app)
+
+const io = new Server(http, {
+  cors: {
+    origin: process.env.CORS_ORIGIN
+  }
+})
 const port = process.env.PORT ?? 3333
 
 app.use(express.json())
@@ -16,5 +25,13 @@ app.use(
   })
 )
 app.use(routes)
+
+io.on('connection', socket => {
+  logger('io', 'connect', 'Socket open!')
+
+  socket.on('message', msg => {
+    console.log(msg)
+  })
+}).on('close', () => logger('io', 'disconnect', 'Socket closed.'))
 
 app.listen(port, () => logger('server', 'listen', `Server is running on ::${port}`))
