ac65300465c44e84254326c606f47e2ec41fb984
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-20 14:48:42 -0300

    refactor: html email body upgraded

--- src/infra/mail/mailtrap/repositories/mailTrapRepository.ts
+++ src/infra/mail/mailtrap/repositories/mailTrapRepository.ts
@@ -9,8 +9,56 @@ export class MailTrapRepository implements MailRepository {
       to: email,
       subject: 'User confirmation',
       html: `
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+        <title>User Activated Successful!</title>
+      </head>
+      <body>
         <h1>Activate your account!</h1>
+        <p>To activate your account:</p>
         <a href="${activateURL}">${activateURL}</a>
+
+        <style>
+          * {
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+          }
+
+          body {
+            width: 100vw;
+            height: 100vh;
+            overflow: hidden;
+
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-direction: column;
+
+            background-color: #1a1924;
+            gap: 10px;
+          }
+
+          body * {
+            font-family: 'Roboto', sans-serif;
+            color: #e1e1e6;
+          }
+
+          a {
+            color: #00b37e;
+          }
+        </style>
+      </body>
+      </html>
       `
     })
   }
