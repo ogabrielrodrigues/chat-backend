3251482d55c37749a864cbed419f7dabd5c6bb00
ogabrielrodrigues <gabrielrodrigues.dev409@gmail.com>
2023-02-19 22:42:58 -0300

    refactor: re-organized imports

--- src/infra/http/instances/userControllerInstance.ts
+++ src/infra/http/instances/userControllerInstance.ts
@@ -1,15 +1,19 @@
 import { UserController } from '@controllers/userController'
-import { prisma } from '@database/prisma/prisma'
-import { SESClient } from '../../mail/aws/ses'
-import { PrismaUserRepository } from '@database/prisma/repositories/prismaUserRepository'
-import { redis } from '@database/redis/redis'
-import { RedisUserRepository } from '@database/redis/repositories/redisUserRepository'
+
 import { CountUsers } from '@useCases/countUsers'
 import { CreateUser } from '@useCases/createUser'
 import { GetUserById } from '@useCases/getUserById'
 import { GetUsers } from '@useCases/getUsers'
 import { UpdateUser } from '@useCases/updateUser'
+import { GenerateConfirmationURL } from '@useCases/generateConfirmationURL'
+
+import { PrismaUserRepository } from '@database/prisma/repositories/prismaUserRepository'
+import { RedisUserRepository } from '@database/redis/repositories/redisUserRepository'
 import { SESRepository } from '../../mail/aws/repositories/sesRepository'
+
+import { prisma } from '@database/prisma/prisma'
+import { redis } from '@database/redis/redis'
+import { SESClient } from '../../mail/aws/ses'
 
 const prismaUserRepository = new PrismaUserRepository(prisma)
 const redisUserRepository = new RedisUserRepository(redis, prismaUserRepository)
@@ -19,7 +23,18 @@ const getUsers = new GetUsers(redisUserRepository)
 const countUsers = new CountUsers(prismaUserRepository)
 const getUserById = new GetUserById(redisUserRepository)
 const updateUser = new UpdateUser(prismaUserRepository)
+const generateConfirmationURL = new GenerateConfirmationURL()
+
 const sesRepository = new SESRepository(SESClient)
-const userController = new UserController(createUser, getUsers, countUsers, getUserById, updateUser, sesRepository)
+
+const userController = new UserController(
+  createUser,
+  getUsers,
+  countUsers,
+  getUserById,
+  updateUser,
+  generateConfirmationURL,
+  sesRepository
+)
 
 export { userController }
